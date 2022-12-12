a92b554928a82b8b0078421cd94aaf898a3c6385
Eiji Toriki <ageForEngi@outlook.jp>
2022-12-12 21:04:47 +0900

    Loginページのハリボテ作成

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import './css/Login.css'
 
 export default function Login() {
+  const handleLogin = () => {
+
+  }
+
+
   return (
-    <div>Login</div>
+    <div className='app-login'>
+      <div className='app-login-card'>
+        <h2 className='app-login-title'>Login from</h2>
+        <div className='app-login-user'>
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div className='app-login-pass'>
+            <label for="pass">Password </label>
+            <input type="password" id="pass" name="password"
+                  minlength="8" required />
+        </div>
+        <button className='app-login-button' onClick={handleLogin}>login</button>
+      </div>
+    </div>
   )
 }
